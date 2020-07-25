6ccc4160763d9553455abc52829ffbe05659a711
conao3 <conao3@gmail.com>
2020-07-25 19:37:45 +0900

    generate project

---
+++ dired-memo.el
@@ -0,0 +1,42 @@
+;;; dired-memo.el --- Add memo to directory and show it in dired  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  Naoya Yamashita
+
+;; Author: Naoya Yamashita <conao3@gmail.com>
+;; Version: 0.0.1
+;; Keywords: convenience
+;; Package-Requires: ((emacs "26.1"))
+;; URL: https://github.com/conao3/dired-memo.el
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Add memo to directory and show it in dired.
+
+
+;;; Code:
+
+(defgroup dired-memo nil
+  "Add memo to directory and show it in dired."
+  :group 'convenience
+  :link '(url-link :tag "Github" "https://github.com/conao3/dired-memo.el"))
+
+(provide 'dired-memo)
+
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; End:
+
+;;; dired-memo.el ends here
