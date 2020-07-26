47a75346a90bf439f303ecfdf35579595fb204a3
conao3 <conao3@gmail.com>
2020-07-26 19:26:48 +0900

    add recommneded customize code

--- dired-lsi.el
+++ dired-lsi.el
@@ -25,6 +25,13 @@
 
 ;; Add memo to directory and show it in dired.
 
+;; To use this package, simply add this to your init.el:
+;;   (add-hook 'dired-mode-hook 'dired-lsi-mode)
+
+;; If you use `dired-git', I suggest below customize.
+;;   (setq dired-lsi--create-spacer-fn
+;;         (lambda () (when all-the-icons-dired-mode "  \t")))
+
 
 ;;; Code:
 
